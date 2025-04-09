d8dda1eabaf62fa7b7937e396dc1dcac98d577a7
Marius Kruger <amanic@gmail.com>
2025-04-09 22:36:21 +0200

    use enum syntax that works in Stipple 0.31.18

--- src/simulations/Simulations.jl
+++ src/simulations/Simulations.jl
@@ -66,9 +66,17 @@ end
 
 function ui()
     [
-        span(view_single(), @if("viewMode == '$VIEW'")),
-        span(view_new(), @elseif("viewMode == '$NEW'")),
-        span(view_list(), @else(true)),
+        # This loads everything onto the screen, but conditionally hides it
+        # span(view_single(), @showif(:viewMode == VIEW)),
+        # span(view_new(), @showif(:viewMode == NEW)),
+        # span(view_list(), @showif(:viewMode == LIST)),
+
+        # this syntax is supposed to load lazily,
+        # but as far as I can tell everything is still loaded
+        # https://vuejs.org/guide/essentials/conditional.html#v-show
+        span(view_single(), @if(:viewMode == VIEW)),
+        span(view_new(), @elseif(:viewMode == NEW)),
+        span(view_list(), @else()),
     ]
 end
 
