29041b807a3ad2a17fa911d14f61f44a2d62564d
Marius Kruger <amanic@gmail.com>
2025-08-16 17:05:56 +0200

    update layout and played around with date picker

--- src/controller_common.jl
+++ src/controller_common.jl
@@ -4,10 +4,18 @@ console.log('This app has just been created!');
 document.querySelector('.layout_root').hidden = false;
 """
 
+"""
+    Redirect to some url path.
+    eg. `@run jsredirect("/some/url/path")`
+"""
 function jsredirect(path)
     return """window.location.href = '$path'"""
 end
 
+"""
+    Notify the user with a message.
+    eg. `@run notifyError("Oh-oh")`
+"""
 function notifyError(message)
     @error message
     return """
--- src/simulations/view_new.jl
+++ src/simulations/view_new.jl
@@ -1,5 +1,7 @@
 # the last statement is returned by the import so
 # this file returns a function that gets loaded by Genie
+FORM_LEFT = 3
+FORM_RIGHT = 9
 function view_new()
     [
         moduleToolbar("Run a new simulation", [
@@ -7,10 +9,21 @@ function view_new()
             btn("List", class="q-mr-sm", @click(:listButton))
         ]),
         row(class = "q-pa-sm", [
-            card(class = "q-ma-sm q-pa-sm col-12 col-md-4", [
+            card(class = "q-pa-sm col-12 col-md-7", [
                 row([
-                    cell("Date range"),
-                    datepicker(:daterange, range = true, todaybtn = true)
+                    cell(size = FORM_LEFT, [p("Date range", class="text-h5"),
+                        p("(End date is exclusive)", class="text-subtitle1")
+                    ]),
+                    datepicker(size = FORM_RIGHT, :daterange, range = true, todaybtn = true,
+                        # title="", # this obscures the summary eg "2 days"
+                        # subtitle="", # this obscures the current month
+                        landscape = true,
+                        # color = "red",
+                        events = ["2025/05/03","2025/05/10"], # TODO: add today and next week
+                        # eventsfn! = "(date) => date === '2025/05/17'", could not get this to work
+                        # eventcolor = "green",
+                        event__color! = "(date) => date[9] % 2 === 0 ? 'green' : 'orange'",
+                        navmaxyearmonth=Dates.format(today(), "yyyy/mm"))
                 ]),
             ])
         ]),
